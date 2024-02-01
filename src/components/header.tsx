e71d9be037e0ddc653300a32870bc3b0e4d0bc2e
Fernanda <febaccarini@gmail.com>
2024-02-01 19:25:54 -0300

    fisrt commit

---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+
+
+function Header(){
+
+    return(
+        <header className="w-full flex flex-col align-center justify-center  ">
+
+            <section className="w-[90%] max-w-[1080px] direction-row  justify-between">
+
+                <div>
+
+                    <h1>Teste</h1>
+
+                </div>
+
+                <div>
+
+                    <nav>
+                        <a href="">Teste</a>
+                        <a href="">Teste</a>
+                        <a href="">Teste</a>
+                    </nav>
+
+                </div>
+
+            </section>
+
+
+        </header>
+    )
+
+}
+
+export default Header;

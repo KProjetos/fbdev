486b06569e51905de96f299a28cbad6233711aa9
Fernanda <febaccarini@gmail.com>
2024-02-20 22:17:26 -0300

    feat. descktop section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
-import Header  from "../components/header"
-import "./globals.css"
+import { titleFont } from "./images/font/title";
+import { textFont } from "./images/font/text";
+import { Button } from "@/components/ui/button"
+import "./globals.css";
 
 export default function Home() {
   return (
     <>
       <section className="w-full flex flex-col items-center justify-center">
-        <div className="w-[90%] mac-w-[1080px] flex flex-row items-center justify-between">
-          <div>teste</div>
-          <div>tesste</div>
+        <div className="w-full flex flex-row items-center justify-between">
+          <div className="w-[50%] px-12 flex flex-col items-center justify-center">
+            <p className={`${titleFont.className} text-5xl mb-10`}>Desenvolvimento de sites e sistemas </p>
+            <p className={`${textFont.className} text-xl`}>Empresa com foco em criação,melhoria e desempenho atendendo a sua necessidade com inovação e estratégia. Agende uma reunião.</p>
+            <div className="flex flex-row items-center justify-between">
+              <Button className="w-[150px] mx-4 my-8 rounded-xl" variant="default">AGENDAR</Button>
+              <Button className="w-[150px] mx-4 my-8 rounded-xl" variant="default">PORTIFÓLIO</Button>
+            </div>
+          </div>
+          <div className="w-[50%]">
+            <img src="https://firebasestorage.googleapis.com/v0/b/fbdev-97ff9.appspot.com/o/site%2FIMG%20HEADER.png?alt=media&token=630f1dd5-ad37-4472-b887-5c85bc2f94c3" alt="" />
+          </div>
         </div>
       </section>
     </>

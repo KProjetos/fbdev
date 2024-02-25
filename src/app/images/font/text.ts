d6a0ca7820aec07c23a303c952955c5ff4addddf
Fernanda <febaccarini@gmail.com>
2024-02-25 17:12:00 -0300

    fix change fonts path

--- src/app/images/font/text.ts
+++
@@ -1,6 +0,0 @@
-import { Montserrat } from 'next/font/google';
-
-export const textFont = Montserrat({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700'],
-});
--- src/app/images/font/title.ts
+++
@@ -1,6 +0,0 @@
-import { Jura } from 'next/font/google';
-
-export const titleFont = Jura({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700'],
-});

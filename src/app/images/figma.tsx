64cb735fbe402672be948f03b790c3fa96ee7f9f
Fernanda <febaccarini@gmail.com>
2024-02-27 21:59:14 -0300

    add images

---
+++ src/app/images/figma.tsx
@@ -0,0 +1,34 @@
+function Figma() {
+  return (
+    <svg
+      width="54"
+      height="54"
+      viewBox="0 0 54 54"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.25 12L27 3.25L35.75 12L27 20.75L18.25 12ZM18.25 42L27 33.25L35.75 42L27 50.75L18.25 42ZM33.25 27L42 18.25L50.75 27L42 35.75L33.25 27ZM3.25 27L12 18.25L20.75 27L12 35.75L3.25 27Z"
+        stroke="url(#paint0_linear_102_173)"
+        stroke-width="5.83333"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_102_173"
+          x1="27"
+          y1="3.25"
+          x2="27"
+          y2="50.75"
+          gradientUnits="userSpaceOnUse"
+        >
+          {/* <stop stop-color="white" /> */}
+          <stop offset="1" stop-color="#0F172A" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default Figma;

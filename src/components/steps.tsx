fffb81d225705cddd822b9983fe2e85b635ecffc
Fernanda <febaccarini@gmail.com>
2024-03-09 15:25:20 -0300

    feat add fonts

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -1,7 +1,6 @@
+import { textFont } from "@/app/font/text";
 import { titleFont } from "@/app/font/title";
 import ArrowTitle from "@/app/images/arrowTitle";
-import Figma from "@/app/images/figma";
-import Firebase from "@/app/images/firebase";
 import React from "@/app/images/react";
 
 function Steps() {
@@ -10,10 +9,10 @@ function Steps() {
       <div className="w-[90%] max-w-[1080px] flex flex-col items-center justify-center mb-8">
         <div className="w-full flex lg:flex-row flex-col items-center justify-center lg:justify-evenly gap-4">
           <div className="w-full h-[125px] px-4 flex flex-col items-start justify-start my-12">
-            <h2 className="uppercase mb-4 text-2xl title-detail pl-2">
+            <h2 className={`${titleFont.className} uppercase mb-4 text-2xl title-detail pl-2 font-bold`}>
               1. Reunião
             </h2>
-            <p>
+            <p className={`${textFont.className}`}>
               Iniciamos nosso processo com uma reunião inicial, onde mergulhamos
               na visão do seu projeto. Durante essa consulta, discutimos suas
               necessidades específicas, metas de negócios e preferências de
@@ -23,10 +22,10 @@ function Steps() {
           </div>
 
           <div className="w-full h-[125px] px-4 flex flex-col items-start justify-start my-12">
-            <h2 className="uppercase mb-4 text-2xl title-detail pl-2">
+            <h2 className={`${titleFont.className} uppercase mb-4 text-2xl title-detail pl-2 font-bold`}>
               2. Design
             </h2>
-            <p>
+            <p className={`${textFont.className}`}>
               Após a reunião inicial, dedicamos tempo à planificação
               estratégica. Nossa equipe analisa os requisitos técnicos,
               identifica as melhores práticas de design e desenvolvimento.
@@ -36,10 +35,10 @@ function Steps() {
 
         <div className="w-full flex lg:flex-row flex-col items-center justify-center lg:justify-evenly gap-4">
           <div className="w-full h-[125px] px-4 flex flex-col items-start justify-start my-12">
-            <h2 className="uppercase mb-4 text-2xl title-detail pl-2">
+            <h2 className={`${titleFont.className} uppercase mb-4 text-2xl title-detail pl-2 font-bold`}>
               3. Codificação
             </h2>
-            <p>
+            <p className={`${textFont.className}`}>
               Com o plano em mãos, nossa equipe de desenvolvimento coloca as
               mãos à obra. Durante esta fase, trabalhamos arduamente para
               traduzir o conceito em realidade, focando na criação de um
@@ -49,10 +48,10 @@ function Steps() {
           </div>
 
           <div className="w-full h-[125px] px-4 flex flex-col items-start justify-start my-12">
-            <h2 className="uppercase mb-4 text-2xl title-detail pl-2">
+            <h2 className={`${titleFont.className} uppercase mb-4 text-2xl title-detail pl-2 font-bold`}>
               4. Site / Sistema
             </h2>
-            <p>
+            <p className={`${textFont.className}`}>
               O sucesso do seu site é nosso objetivo final. Após a
               implementação, conduzimos testes extensivos para garantir que tudo
               esteja funcionando perfeitamente. Desde a navegação do usuário até
@@ -70,7 +69,7 @@ function Steps() {
               src="https://firebasestorage.googleapis.com/v0/b/fbdev-97ff9.appspot.com/o/site%2Freuniao.png?alt=media&token=6c12433e-9812-47b9-ba04-1da3093143f9"
               alt="Reunião"
             />
-            <p>Reunião</p>
+            <p className={`${titleFont.className} text-[1.2em] mt-1`}>Reunião</p>
           </div>
           <div className="w-full flex flex-col items-center justify-center">
             <img
@@ -78,7 +77,7 @@ function Steps() {
               src="https://firebasestorage.googleapis.com/v0/b/fbdev-97ff9.appspot.com/o/site%2Fdesign.png?alt=media&token=c9a2d3c7-56df-433e-be35-9be20ecff05c"
               alt=""
             />
-            <p>Design</p>
+            <p className={`${titleFont.className} text-[1.2em] mt-1`}>Design</p>
           </div>
           <div className="w-full flex flex-col items-center justify-center">
             <img
@@ -86,7 +85,7 @@ function Steps() {
               src="https://firebasestorage.googleapis.com/v0/b/fbdev-97ff9.appspot.com/o/site%2Fprogramacao%201.png?alt=media&token=2f76b12e-fefa-4f48-a517-a1c4bb9e1a75"
               alt=""
             />
-            <p>Codificação</p>
+            <p className={`${titleFont.className} text-[1.2em] mt-1`}>Codificação</p>
           </div>
           <div className="w-full flex flex-col items-center justify-center">
             <img
@@ -94,7 +93,7 @@ function Steps() {
               src="https://firebasestorage.googleapis.com/v0/b/fbdev-97ff9.appspot.com/o/site%2Fsite.png?alt=media&token=d7df4a22-eabe-40d5-ae1f-2760b0cabae2"
               alt=""
             />
-            <p>Site</p>
+            <p className={`${titleFont.className} text-[1.2em] mt-1`}>Site</p>
           </div>
         </div>
       </div>
@@ -109,20 +108,20 @@ function Steps() {
 
         <div className="w-full flex lg:flex-row flex-col items-center justify-center lg:justify-evenly gap-1">
           <div className="w-full h-[125px] px-4 flex flex-col items-start justify-start my-12">
-            <h2 className="uppercase mb-4 text-2xl pl-2">
+            <h2 className={`${titleFont.className} uppercase mb-4 text-2xl pl-2 font-bold`}>
               Design
             </h2>
-            <p>
+            <p className={`${textFont.className}`}>
               Escolhemos o Figma como nossa ferramenta principal para o design
               do seu projeto. Aonde poderá ver o design em tempo real
             </p>
           </div>
 
           <div className="w-full h-[125px] px-4 flex flex-col items-start justify-start my-12">
-            <h2 className="uppercase mb-4 text-2xl pl-2">
+            <h2 className={`${titleFont.className} uppercase mb-4 text-2xl pl-2 font-bold`}>
               Site
             </h2>
-            <p>
+            <p className={`${textFont.className}`}>
               Ambiente de Teste, um espaço dedicado para que você acompanhe
               diariamente o progresso do seu site de forma direta e envolvente.
             </p>
